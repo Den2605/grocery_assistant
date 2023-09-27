43648c6cf645e00506648b5dae09b16bc163f2a6
Den2605 <rezerv50@yandex.ru>
2023-09-27 15:50:12 +0300

    Test

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv("SECRET_KEY", default="secret12345")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [
     "51.250.108.248",
