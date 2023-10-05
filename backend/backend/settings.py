2e44372c6c1fa25fd47d509918e8d0e066a48bea
Den2605 <rezerv50@yandex.ru>
2023-10-05 16:20:17 +0300

    fix permission

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -123,7 +123,7 @@ DJOSER = {
     "LOGIN_FIELD": "email",
     "PERMISSIONS": {
         "user_list": ["rest_framework.permissions.AllowAny"],
-        "user": ["rest_framework.permissions.IsAuthenticated"],
+        "user": ["rest_framework.permissions.AllowAny"],
     },
     "SERIALIZERS": {
         "user": "api.serializers.CustomUserSerializer",
